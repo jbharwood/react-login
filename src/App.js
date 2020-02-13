29cc52d00453d7af7092fb32c708e40860ca4a5a
github username <josephblakeharwood@gmail.com>
2020-02-12 19:38:20 -0500

    finished login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Form, Button } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	state = {
+		username: "",
+		password: "",
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	postUser = () => {
+		fetch(`http://localhost:3000/api/v1/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accepts": "application/json",
+			},
+			body: JSON.stringify(this.state)
+		})
+		.then(res => res.json())
+		.then((response) => {
+			if (response.errors) {
+				alert(response.errors)
+			} else {
+				debugger
+				// this.props.setCurrentUser(response.user)
+				// this.props.dispatch({type: "SET_CURRENT_USER", payload: response.user})
+				// localStorage.setItem('jwt', response.jwt)
+				// this.props.history.push(`/`)
+				// this.postToFeedUsers(response.user)
+			}
+		})
+	}
+
+	handleSubmit = () => {
+		this.postUser()
+	}
+
+	render(){
+		return (
+			<div className="top" ref={this.topRef}>
+			<Form onSubmit={this.handleSubmit}>
+		    <Form.Field>
+		      <label>Username</label>
+		      <input onChange={this.handleChange} name="username" value={this.state.username} placeholder='Username' />
+		    </Form.Field>
+		    <Form.Field>
+		      <label>Password</label>
+		      <input onChange={this.handleChange} type="password" name="password" value={this.state.password} placeholder='Password' />
+		    </Form.Field>
+		    <Button type='submit'>Log In</Button>
+		  </Form>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
